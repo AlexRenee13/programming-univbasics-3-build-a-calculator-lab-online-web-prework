39ed82e3b23a38ba0119931082b1a74d00145880
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-27 06:43:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,10 +1,10 @@
 def addition(num1, num2)
-  return num1+num2
+  return num2 + num1
 end
 
 
 def subtraction(num1, num2)
-  return  num1 - num2
+  return  num2 - num1
 end
 
 def division(num1, num2)
