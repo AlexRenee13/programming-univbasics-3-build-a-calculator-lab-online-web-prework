b7220267f5c47b593662baacba0a0269b9cbcdbf
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-27 06:35:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,26 @@
 def addition(num1, num2)
-
+  results= num2 + num1
+puts results
 end
 
-def subtraction(num1, num2)
 
+def subtraction(num1, num2)
+  results= num1 - num2
+puts results
 end
 
 def division(num1, num2)
-
+  results= num1 / num2
+puts results
 end
 
 def multiplication(num1, num2)
-
+  results num1*num2
 end
 
 def modulo(num1, num2)
-
+remainder= num2%num1
+puts remainder
 end
 
 def square_root(num)
